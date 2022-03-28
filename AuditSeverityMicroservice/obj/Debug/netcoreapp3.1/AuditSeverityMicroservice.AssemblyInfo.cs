f334a9b64dcf9eff52e7575d6a4b9ce9e63eb6cb
Humnabad <2085700@cognizant.com>
2022-03-28 09:39:47 +0530

    docker added

--- AuditSeverityMicroservice/obj/Debug/netcoreapp3.1/AuditSeverityMicroservice.AssemblyInfo.cs
+++ AuditSeverityMicroservice/obj/Debug/netcoreapp3.1/AuditSeverityMicroservice.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("ce2e457e-7ead-4082-9c79-c1546502fca8")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditSeverityMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
